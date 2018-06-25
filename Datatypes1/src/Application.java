5079ed99affe6a9f61c9726fc61136f558d724c1
bharatbhamare <32448469+bharatbhamare@users.noreply.github.com>
2018-06-25 23:25:18 +0530

    Update Application.java

--- Datatypes1/src/Application.java
+++ Datatypes1/src/Application.java
@@ -1,4 +1,59 @@
 
+
 public class Application {
 
+	public static void main(String[] args)
+	{
+		// variable declaration  
+		
+		byte byteType;
+		short sortType;
+		int integetrType;
+		long longType;
+		
+		float floatType;
+		double dubleType;
+		
+		
+		boolean booleanType;
+		
+		char charType;
+	// end of variabl declaration  
+		
+		
+	// variable definationm
+		
+		byteType=10;
+		sortType=20;
+		integetrType = 40;
+		longType = 300;
+		
+		floatType= 2.30f; // afrer value f must be added if  f  is not then it considers double
+		dubleType =333.333;
+		
+		booleanType = true;
+		
+		charType='c' ;
+//end of variable defination
+		
+		
+//display
+		
+		System.out.println("byte data: "+ byteType); 
+		System.out.println("short data: "+sortType);
+		System.out.println("integer data: "+integetrType);
+		System.out.println("long data: "+longType);
+		System.out.println("float typoe: "+floatType);
+		System.out.println("double type: "+dubleType);
+		System.out.println("boolean type: "+booleanType);
+		System.out.println("char type: "+charType);
+		
+		
+		
+		
+		
+		
+		
+		
+	}
 }
